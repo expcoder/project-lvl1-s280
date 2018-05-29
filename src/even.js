14031c68cbe93e15b26fb2334a8f9ab3c7451870
expcoder <g.experiens@gmail.com>
2018-05-30 00:56:37 +0500

    fix mentor remarks
    - put some functions into a helpers file
    - put game function into a separate file
    - remove excess code from brain-even file
    - refactor brain-even game

---
+++ src/even.js
@@ -0,0 +1,32 @@
+import readlineSync from 'readline-sync';
+import { getRandomInt, isEvenNum } from './helpers';
+
+const minRange = 1;
+const maxRange = 100;
+const numbersOfAttempts = 3;
+
+const brainEven = () => {
+  console.log('Welcome to Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!\n`);
+  let i = 0;
+  for (i; i < numbersOfAttempts; i += 1) {
+    const question = getRandomInt(minRange, maxRange);
+    console.log(`Question: ${question}`);
+    const answer = isEvenNum(question) ? 'yes' : 'no';
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (answer === userAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      break;
+    }
+  }
+  if (i === numbersOfAttempts) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+
+export default brainEven;
---
+++ src/helpers.js
@@ -0,0 +1,4 @@
+export const getRandomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min)) + min;
+
+export const isEvenNum = num => (num % 2 === 0);
